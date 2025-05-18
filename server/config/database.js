c6df38defe7a2510b25bd329c6047fe2a345d6ba
Hani Fayed <hani.fayed@outlook.com>
2025-05-18 17:02:51 +0300

    Fix syntax error in database.js by adding a missing comma

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: '127.0.0.1',
     port: 3307,
-    user: 'root'
+    user: 'root',
     password: 'MySQL@root',
     database: 'todoing_app',
 }).promise()
