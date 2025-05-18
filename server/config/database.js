13775f0e3122e441a214b83db7366ca492b2a717
Hani Fayed <hani.fayed@outlook.com>
2025-05-18 15:58:17 +0300

    Add database configuration and package files for MySQL connection

---
+++ server/config/database.js
@@ -0,0 +1,8 @@
+import mysql from 'mysql2';
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: 3307,
+    user: 'root'
+    password: 'MySQL@root',
+    database: 'todoing_app',
+}).promise()
