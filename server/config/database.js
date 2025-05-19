058e37272963d91ba33a57ca875c0a215ddb2bb5
Hani Fayed <hani.fayed@outlook.com>
2025-05-19 23:47:23 +0300

    Update database configuration to use environment variables and add dotenv dependency

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,20 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+// Load environment variables from .env file
+dotenv.config({path: '../../.env'});
+
+
+// Create a connection pool
 const pool = mysql.createPool({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: 'MySQL@root',
-    database: 'todoing_app',
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_NAME
 }).promise()
 
+// Test the connection
 async function testDBConnection() {   
     const [result] = await pool.query('SELECT * FROM todo');
     console.log(result);
