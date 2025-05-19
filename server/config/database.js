d9e7ea48a25169b1229686770331de5ec463a729
Hani Fayed <hani.fayed@outlook.com>
2025-05-19 22:51:46 +0300

    Refactor database.js to use CommonJS syntax and update host to 'localhost'; remove 'type' field from package.json

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
-import mysql from 'mysql2';
+const mysql = require('mysql2');
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3307,
     user: 'root',
     password: 'MySQL@root',
