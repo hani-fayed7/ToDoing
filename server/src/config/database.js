e1259f3873160c273c9f787e207f7fd198f34ca0
Hani Fayed <hani.fayed@outlook.com>
2025-05-27 00:46:37 +0300

    refactor: remove unused database query functions to streamline code

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -22,30 +22,3 @@ pool.on('error', (err) => {
 });
 
 export default pool
-
-// Test the connection
-export async function testDBConnection() {   
-    const result = await pool.query('SELECT * FROM todos')
-    return result.rows
-}
-
-// Test the select query with a parameter
-export async function todo(id){
-    const result = await pool.query(`
-        SELECT * 
-        FROM todos
-        WHERE id = $1
-        `, [id]);
-    return result.rows[0]
-    
-}
-
-export async function create_todo(user_id, title, description) {
-    const result = await pool.query(`
-        INSERT INTO todos (user_id, title, description)
-        VALUES ($1, $2, $3)
-        RETURNING *
-    `, [user_id, title, description]);
-    const id = result.rows[0].id
-    return await todo(id)
-}
