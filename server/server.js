966c3e1c647d0123a24d378ccb97b0e495fd39e8
Hani Fayed <hani.fayed@outlook.com>
2025-05-27 00:28:24 +0300

    refactor: update server.js to use port from config object

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import app from './src/app.js';
-const PORT = process.env.PORT || 3000;
+import config from './src/config/config.js';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const {server } = config;
+
+app.listen(server.port, () => {
+  console.log(`Server running on port ${server.port}`);
 });
