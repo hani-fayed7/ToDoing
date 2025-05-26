f6e27c02eaa80f7826353d2eaecb508416070625
Hani Fayed <hani.fayed@outlook.com>
2025-05-27 00:54:02 +0300

    style: remove unnecessary semicolons and ensure consistent formatting in server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import app from './src/app.js';
-import config from './src/config/config.js';
+import app from './src/app.js'
+import config from './src/config/config.js'
 
-const {server } = config;
+const {server } = config
 
 app.listen(server.port, () => {
-  console.log(`Server running on port ${server.port}`);
-});
+  console.log(`Server running on port ${server.port}`)
+})
