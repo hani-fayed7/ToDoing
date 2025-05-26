800785477e404b00fa77a5c1805f8b7f519f76f6
Hani Fayed <hani.fayed@outlook.com>
2025-05-26 23:52:45 +0300

    refactor: update server structure and improve database import handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,26 @@
 import express from 'express'
-import {testDBConnection, todo, create_todo} from './config/database.js'
+import * as db from './config/database.js'
 
 const app = express()
 app.use(express.json())
 
 // Get all todos
 app.get("/todo", async (req, res) => {
-    const todos = await testDBConnection()
+    const todos = await db.testDBConnection()
     res.send(todos)
 });
 
 // Get a specific todo by ID
 app.get("/todo/:id", async (req, res) => {
     const id = req.params.id
-    const todos = await todo(id)
+    const todos = await db.todo(id)
     res.send(todos)
 });
 
 // Create a new todo
 app.post("/todo", async (req, res) => {
     const {user_id, title, description} = req.body
-    const createTodo = await create_todo(user_id, title, description)
+    const createTodo = await db.create_todo(user_id, title, description)
     res.status(201).send(createTodo)
 });
 
@@ -30,5 +30,5 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!')
 });
 
-
-app.listen(3000, () => console.log('Server running on port 3000'))
+// Export the app for use in server/server.js
+export default app
